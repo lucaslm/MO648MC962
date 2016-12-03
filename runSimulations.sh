9a1dc5f002a215f856fc59b8c48cbdd5d67dfd37
Lucas Lustosa Madureira (MO648) <lmadureira@kratos.(none)>
2016-12-03 12:53:28 -0200

    Supporting plotting data without error bars

--- runSimulations.sh
+++ runSimulations.sh
@@ -19,7 +19,8 @@ nsPath=~/ns-allinone-2.35/ns-2.35/ns
 protocols=("tcp" "dctcp" "dccp")
 durations=("1s" "10ms")
 nSenders=(05 10 15 20)
-nSamples=5
+# Default number of samples is 5
+nSamples=$(if [ $# -gt 0 ]; then echo $1; else echo 5; fi)
 
 for protocol in "${protocols[@]}"
 do
@@ -28,7 +29,7 @@ do
     for n in "${nSenders[@]}"
     do
       # For every scenario, run a simulation and create a trace file
-      echo "Taking samples for protocol $protocol with $n senders during a $duration interval"
+      echo "Taking $nSamples sample(s) for protocol $protocol with $n senders during a $duration interval"
       i=0
       outDir=out/$protocol/$duration/${n}Senders
       while [ $((i+=1)) -le $nSamples ]
@@ -45,27 +46,38 @@ do
     truncate -s 0 $csvFile
     for n in "${nSenders[@]}"
     do
-      i=0
-      samples=()
-      sampleSum=0
-      outDir=out/$protocol/$duration/${n}Senders
       echo -n "${n} " >> $csvFile
-      while [ $((i+=1)) -le $nSamples ]
-      do
-        sample=$(awk -f throughput.awk $outDir/trace$i.tr)
-        samples+=($sample)
-        sampleSum=$(bc -l <<< "$sampleSum+$sample")
-        echo -n "$sample " >> $csvFile
-      done
-      # Compute and print mean and 95% confidence interval for this row
-      mean=$(bc -l <<< "$sampleSum/$nSamples")
-      std_dev=$(sd $samples $mean)
-      error=$(bc -l <<< "1.96*$std_dev/sqrt($nSamples)")
-      echo -n $mean" " >> $csvFile
-      echo    $error    >> $csvFile 
+      traceDir=out/$protocol/$duration/${n}Senders
+      if [ $nSamples -gt 1 ]
+      then
+        i=0
+        samples=()
+        sampleSum=0
+        while [ $((i+=1)) -le $nSamples ]
+        do
+          sample=$(awk -f throughput.awk $traceDir/trace$i.tr)
+          samples+=($sample)
+          sampleSum=$(bc -l <<< "$sampleSum+$sample")
+          echo -n "$sample " >> $csvFile
+        done
+        # Compute and print mean and 95% confidence interval for this row
+        mean=$(bc -l <<< "$sampleSum/$nSamples")
+        std_dev=$(sd $samples $mean)
+        error=$(bc -l <<< "1.96*$std_dev/sqrt($nSamples)")
+        echo -n $mean" " >> $csvFile
+        echo    $error   >> $csvFile
+      else
+        # Print the only single value for this row
+        echo $(awk -f throughput.awk $traceDir/trace1.tr) >> $csvFile
+      fi
     done
     # Plot Graphics
-    gnuplot -e "dataFile='$csvFile'; outPath='$imgFile'; yColumn=$(($nSamples+2))" throughput.gpi
+    if [ $nSamples -gt 1 ]
+    then
+      gnuplot -e "dataFile='$csvFile'; outPath='$imgFile'; yColumn=$(($nSamples+2)); errorColumn=$(($nSamples+3))" throughput.gpi
+    else
+      gnuplot -e "dataFile='$csvFile'; outPath='$imgFile';" throughput.gpi
+    fi
   done
 done
 
